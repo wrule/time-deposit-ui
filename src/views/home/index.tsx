f7f87d29d733f72dfefe9ba7f77dedcae01b4ccd
jimao <1982775886@qq.com>
2021-05-28 16:51:28 +0800

    提交

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,9 +3,18 @@ import { VNode } from 'vue';
 import style from './index.module.scss';
 import logo from '@/assets/logo.png';
 import XHello from '@/components/hello';
+import Web3 from 'web3';
+import Deposit from './Deposit.json';
 
 @Component
 export default class ViewHome extends Vue {
+
+  private mounted() {
+    const web3 = new Web3('ws://127.0.0.1:7545');
+    const depositContract = new web3.eth.Contract(Deposit as any, "");
+    
+  }
+
   public render(): VNode {
     return (
       <div class={style.view}>
